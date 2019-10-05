084990038a16e123e2400bcf70af3b435266b56b
jonathan <jonathanoliveirasilva9@gmail.com>
2019-10-05 19:58:43 -0300

    Stateless and Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,88 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: 'Count People',
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/restaurant.jpg",
-            fit : BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "People: 0",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        '+1',
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      ),
-                      onPressed: () {},
+  runApp(MaterialApp(title: 'Count People', home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = "You can come in";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+
+      if (_people < 0) {
+        _infoText = "Invert World";
+      } else if (_people <= 10) {
+        _infoText = "You can come in";
+      } else {
+        _infoText = "Full";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "People: $_people",
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      '+1',
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        '-1',
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      ),
-                      onPressed: () {},
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      '-1',
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
                   ),
-                ],
-              ),
-              Text(
-                "Welcome!!!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 30.0),
-              )
-            ],
-          ),
-        ],
-      )));
+                ),
+              ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        ),
+      ],
+    );
+  }
 }
