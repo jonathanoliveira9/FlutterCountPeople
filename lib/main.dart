268f9b8edd5e1158f0fdae91dd1325c26e28b28a
jonathan <jonathanoliveirasilva9@gmail.com>
2019-10-05 18:58:18 -0300

    Image, Padding, Stack

--- lib/main.dart
+++ lib/main.dart
@@ -3,38 +3,55 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: 'Count People',
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text(
-            "People: 0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          Image.asset(
+            "images/restaurant.jpg",
+            fit : BoxFit.cover,
+            height: 1000.0,
           ),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlatButton(
-                child: Text('+1',
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
+              Text(
+                "People: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
-
-              FlatButton(
-                child: Text('-1',
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        '+1',
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        '-1',
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
+              Text(
+                "Welcome!!!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
+              )
             ],
           ),
-          Text(
-            "Welcome!!!",
-            style: TextStyle(
-                color: Colors.deepPurple,
-                fontStyle: FontStyle.italic,
-                fontSize: 30.0),
-          )
         ],
       )));
 }
