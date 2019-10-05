d3ac1acc005b4a1c252089ff43a12e614bd85c98
jonathan <jonathanoliveirasilva9@gmail.com>
2019-10-05 16:54:39 -0300

    Column and Text

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Count People',
-    home: Container(color: Colors.white,)
-  ));
-
+      title: 'Count People',
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "People: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Welcome!!!",
+            style: TextStyle(
+                color: Colors.deepPurple,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
+          )
+        ],
+      )));
 }
